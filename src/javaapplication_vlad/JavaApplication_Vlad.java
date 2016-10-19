7ee3cb63682c8cadf09d99406934704221ca9454
2160489 <2160489@C2-LAI2-M02.IPLeiria.pt>
2016-10-19 16:06:03 +0100

    Escrevi uma linha de código

---
+++ src/javaapplication_vlad/JavaApplication_Vlad.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication_vlad;
+
+/**
+ *
+ * @author 2160489
+ */
+public class JavaApplication_Vlad {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main (String[] args) {
+    
+        System.out.println("Olá Programação! - Vlad");
+    }
+    
+   
+    
+}
